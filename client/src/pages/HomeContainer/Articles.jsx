5f82f2d92aec45b2e3ebde9ae9b83725b2e6e900
aguapawan <agupawankumar45@gmail.com>
2024-02-04 22:28:35 +0530

    Added filter by user checkbox

--- client/src/pages/HomeContainer/Articles.jsx
+++ client/src/pages/HomeContainer/Articles.jsx
@@ -2,44 +2,93 @@ import React, { useState, useEffect } from "react";
 import { useGetCollectionsQuery } from "../../services/jsonServerApi";
 import ArticleCard from "../../components/ArticleCard";
 import { FaArrowRight } from "react-icons/fa";
-import { useSelector } from 'react-redux';
-import { selectSearchQuery } from '../../store/reducers/searchReducers';
+import { useSelector } from "react-redux";
+import { selectSearchQuery } from "../../store/reducers/searchReducers";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Articles = () => {
-  const { isLoading, isError, isSuccess, data, error } = useGetCollectionsQuery();
+  const { isLoading, isError, isSuccess, data, error } =
+    useGetCollectionsQuery();
+  const userState = useSelector((state) => state.user);
   const searchQuery = useSelector(selectSearchQuery);
   const [visibleArticles, setVisibleArticles] = useState(3);
   const [filteredData, setFilteredData] = useState(null);
+  const [filterByUser, setFilterByUser] = useState(false);
+  const [filteredArticlesData, setFilteredArticlesData] = useState(data || []);
 
   const loadMoreArticles = () => {
     setVisibleArticles((prevVisibleArticles) => prevVisibleArticles + 3);
   };
 
+  const handleCheckboxChange = () => {
+    setFilterByUser((prevFilterByUser) => !prevFilterByUser);
+    let flag = !filterByUser;
+    if (userState.userInfo && flag) {
+      
+      const tempData = data?.filter(
+         (collection) => collection?.userId === userState?.userInfo._id
+       );
+
+       setFilteredArticlesData(tempData);
+   }
+   else{
+     setFilteredArticlesData(data);
+   }
+   
+  };
+
+  useEffect(() => {
+    setFilteredArticlesData(data);
+  }, [data]);
+
   useEffect(() => {
     
-    // Reset visible articles to 6 when data changes
-     setVisibleArticles(6);
+    setVisibleArticles(6);
 
-     if (searchQuery) {
-      const filteredArticles = data?.filter((collection) =>
-        collection?.name.toLowerCase().includes(searchQuery.toLowerCase()) || collection?.description.toLowerCase().includes(searchQuery.toLowerCase())
+
+    if (searchQuery) {
+      const filteredArticles = filteredArticlesData?.filter(
+        (collection) =>
+          collection?.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          collection?.description
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
       );
       setFilteredData(filteredArticles);
     } else {
       setFilteredData(null);
     }
-  
-    
-  }, [data, searchQuery]);
+  }, [data, searchQuery, filterByUser]);
 
   return (
     <section className="container mx-auto flex flex-col md:gap-x-5 gap-y-5 px-5 py-10 bg-dark-hard">
+      {userState.userInfo ? (
+        <div className="flex items-center mb-3">
+        <input
+          type="checkbox"
+          id="filterByUserCheckbox"
+          className="form-checkbox h-6 w-6 text-primary rounded border-primary focus:ring-primary"
+          checked={filterByUser}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="filterByUserCheckbox" className="ml-3 text-lg font-semibold text-white">
+          Filter by User
+        </label>
+      </div>
+      
+      ) : null}
+
       <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
-        {(filteredData || data)?.slice(0, visibleArticles).map((collection,index) => (
-          <ArticleCard key={index} className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" collection={collection} />
-        ))}
+        {(filteredData || filteredArticlesData)
+          ?.slice(0, visibleArticles)
+          .map((collection, index) => (
+            <ArticleCard
+              key={index}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+              collection={collection}
+            />
+          ))}
       </div>
 
       {visibleArticles < data?.length && (
